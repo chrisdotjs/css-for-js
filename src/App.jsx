47bb38bfbcae569110ffd80f68836e9d56158dc7
chrisdotjs <chrisdotjs@gmail.com>
2023-01-30 15:24:17 +0100

    Add h2 h3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Module0 from "./components/module0/Module0";
 function App() {
   return (
     <div>
+      <h2>CSS for JS</h2>
+      <h3>* React SEO</h3>
       <Module0 />
     </div>
   );
